e7c610ed1e479e55540aee252e2f72599913e283
Abigail Vega <abigail240396@hotmail.com>
2017-11-28 23:25:34 -0600

    listar notas

--- app/src/main/java/abigail/cortez/vega/nooteshared/MainActivity.java
+++ app/src/main/java/abigail/cortez/vega/nooteshared/MainActivity.java
@@ -13,14 +13,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    ListView listaNotas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listaNotas=(ListView) findViewById(R.id.listarNotas);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
